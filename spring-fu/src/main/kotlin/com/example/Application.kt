7569c94248f1c0f8446ce217bd0131ba01bf741a
hirakida <hirakida29@gmail.com>
2019-03-09 18:43:43 +0900

    Update demo

--- spring-fu/src/main/kotlin/com/example/Application.kt
+++ spring-fu/src/main/kotlin/com/example/Application.kt
@@ -1,42 +1,28 @@
 package com.example
 
-import org.springframework.boot.kofu.application
-import org.springframework.boot.kofu.web.jackson
-import org.springframework.boot.kofu.web.server
-import org.springframework.http.MediaType
+import org.springframework.fu.kofu.web.server
+import org.springframework.fu.kofu.webApplication
 import org.springframework.web.reactive.function.BodyInserters
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.router
-import java.time.LocalDateTime
 
-val app = application {
+
+val app = webApplication {
     server {
         port = if (profiles.contains("test")) 8181 else 8080
         codecs {
             string()
             jackson()
         }
-        import(::routes)
+        router {
+            GET("/") {
+                ok().syncBody("Hello, Spring Fu!")
+            }
+            GET("/json") {
+                ok().body(BodyInserters.fromObject(mapOf("message" to "Hello!")))
+            }
+        }
     }
 }
 
-fun routes() = router {
-    GET("/") {
-        ServerResponse
-                .ok()
-                .syncBody("Hello!")
-    }
-    GET("/api") {
-        ServerResponse
-                .ok()
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .body(BodyInserters.fromObject(Response(message = "Hello!", datetime = LocalDateTime.now())))
-    }
+fun main() {
+    app.run()
 }
-
-data class Response(
-        val message: String,
-        val datetime: LocalDateTime
-)
-
-fun main(): Unit = app.run()
