17c06bea23580179a5db2b06d5b4e3e9b8c048a4
hirakida <hirakida29@gmail.com>
2019-03-24 21:29:44 +0900

    Update oauth2 demo

--- spring-security/security-oauth2/oauth2-github/src/main/java/com/example/ApiController.java
+++ spring-security/security-oauth2/oauth2-github/src/main/java/com/example/ApiController.java
@@ -1,15 +1,47 @@
 package com.example;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.client.GitHubApiClient;
+import com.example.model.Repo;
+import com.example.model.User;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 class ApiController {
+    private final OAuth2RestTemplate restTemplate;
+    private final GitHubApiClient gitHubApiClient;
 
     @GetMapping("/")
     public OAuth2Authentication index(@AuthenticationPrincipal OAuth2Authentication authentication) {
         return authentication;
+    }
+
+    @GetMapping("/access_token")
+    public OAuth2AccessToken getAccessToken() {
+        return restTemplate.getAccessToken();
+    }
+
+    @GetMapping("/resource")
+    public OAuth2ProtectedResourceDetails getResource() {
+        return restTemplate.getResource();
+    }
+
+    @GetMapping("/user")
+    public User getUser() {
+        return gitHubApiClient.getUser();
+    }
+
+    @GetMapping("/repos")
+    public Repo[] getRepos() {
+        return gitHubApiClient.getRepos();
     }
 }
--- spring-security/security-oauth2/oauth2-github/src/main/java/com/example/config/SecurityConfig.java
+++ spring-security/security-oauth2/oauth2-github/src/main/java/com/example/config/SecurityConfig.java
@@ -1,10 +1,14 @@
-package com.example;
+package com.example.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -22,5 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .logoutSuccessUrl("/")
             .deleteCookies("JSESSIONID")
             .invalidateHttpSession(true);
+    }
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext,
+                                                 OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
     }
 }
