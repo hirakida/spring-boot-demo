dd24df0d202bd8571b85243f305ae2da61fa466f
hirakida <12070156+hirakida@users.noreply.github.com>
2019-11-07 06:50:37 +0100

    Update

--- spring-webflux/spring-webflux/src/main/java/com/example/ApiController.java
+++ spring-webflux/spring-webflux/src/main/java/com/example/ApiController.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Map;
-import java.util.Random;
 import java.util.stream.Stream;
 
 import org.springframework.http.MediaType;
@@ -17,22 +16,21 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class ApiController {
-    private static final Random random = new Random();
 
     @GetMapping("/date")
     public Mono<Map<String, LocalDate>> date() {
         return Mono.just(Map.of("date", LocalDate.now()));
     }
 
-    @GetMapping(path = "/datetime", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    public Flux<Map<String, LocalDateTime>> datetimeWithStream() {
-        return Flux.fromStream(Stream.generate(() -> Map.of("datetime", LocalDateTime.now())))
+    @GetMapping(path = "/time", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<LocalTime> time() {
+        return Flux.fromStream(Stream.generate(LocalTime::now))
                    .delayElements(Duration.ofSeconds(1));
     }
 
-    @GetMapping(path = "/time", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<LocalTime> random() {
-        return Flux.fromStream(Stream.generate(LocalTime::now))
+    @GetMapping(path = "/datetime", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Map<String, LocalDateTime>> datetime() {
+        return Flux.fromStream(Stream.generate(() -> Map.of("datetime", LocalDateTime.now())))
                    .delayElements(Duration.ofSeconds(1));
     }
 }
