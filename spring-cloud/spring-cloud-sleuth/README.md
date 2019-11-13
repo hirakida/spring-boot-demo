37c69331b2bfcf1df371ed6706735de92c1b5f50
hirakida <12070156+hirakida@users.noreply.github.com>
2019-11-13 23:05:53 +0900

    Add spring cloud sleuth

---
+++ spring-cloud/spring-cloud-sleuth/README.md
@@ -0,0 +1,2 @@
+
+https://cloud.spring.io/spring-cloud-sleuth/single/spring-cloud-sleuth.html
---
+++ spring-cloud/spring-cloud-sleuth/src/main/java/com/example/ApiController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public Map<String, Object> getDataTime() {
+        return Map.of("datetime", LocalDateTime.now());
+    }
+}
