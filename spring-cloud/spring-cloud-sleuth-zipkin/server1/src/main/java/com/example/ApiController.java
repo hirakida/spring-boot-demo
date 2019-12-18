dc3a8b9f8fbe6398bea187a74374909b4e1ba70a
hirakida <12070156+hirakida@users.noreply.github.com>
2019-12-18 23:42:29 +0900

    Update

--- spring-cloud/spring-cloud-sleuth-zipkin/server1/src/main/java/com/example/ApiController.java
+++ spring-cloud/spring-cloud-sleuth-zipkin/server1/src/main/java/com/example/ApiController.java
@@ -3,7 +3,6 @@ package com.example;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +10,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class ApiController {
     private final RestTemplate restTemplate;
     private final UserRepository userRepository;
 
-    public ApiController(RestTemplateBuilder builder, UserRepository userRepository) {
-        restTemplate = builder.build();
-        this.userRepository = userRepository;
-    }
-
     @GetMapping("/datetime")
     public LocalDateTime getDateTime() {
-        return restTemplate.getForObject("http://localhost:8081/datetime",
-                                         LocalDateTime.class);
+        return restTemplate.getForObject("http://localhost:8081/datetime", LocalDateTime.class);
     }
 
     @GetMapping("/users")
@@ -35,7 +31,6 @@ public class ApiController {
     @GetMapping("/github/users/{username}")
     public JsonNode getGitHubUser(@PathVariable String username) {
         return restTemplate.getForObject("https://api.github.com/users/{username}",
-                                         JsonNode.class,
-                                         username);
+                                         JsonNode.class, username);
     }
 }
--- spring-cloud/spring-cloud-sleuth-zipkin/server1/src/main/java/com/example/Server1Application.java
+++ spring-cloud/spring-cloud-sleuth-zipkin/server1/src/main/java/com/example/Server1Application.java
@@ -2,11 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class Server1Application {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Server1Application.class, args);
