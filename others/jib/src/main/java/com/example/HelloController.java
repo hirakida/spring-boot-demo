de4f6549eeb6f0eb205570bba5a614a788db909f
hirakida <12070156+hirakida@users.noreply.github.com>
2020-05-04 10:56:18 +0900

    Update

--- others/jib/src/main/java/com/example/HelloController.java
+++ others/jib/src/main/java/com/example/HelloController.java
@@ -1,20 +1,12 @@
 package com.example;
 
-import java.time.LocalDateTime;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ApiController {
-
+public class HelloController {
     @GetMapping
     public String hello() {
         return "Hello, Jib!";
-    }
-
-    @GetMapping("/datetime")
-    public LocalDateTime datetime() {
-        return LocalDateTime.now();
     }
 }
