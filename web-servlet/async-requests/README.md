5195fb4950a19b71b202cf716a51945f2888b73b
hirakida <12070156+hirakida@users.noreply.github.com>
2020-09-12 23:13:42 +0900

    Update

--- web-servlet/async-requests/README.md
+++ web-servlet/async-requests/README.md
@@ -1,2 +1,2 @@
 
-https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-async
+https://docs.spring.io/spring/docs/5.2.8.RELEASE/spring-framework-reference/web.html#mvc-ann-async-reactive-types  
--- web-servlet/async-requests/rxjava2/src/main/java/com/example/RxJava2Controller.java
+++ web-servlet/async-requests/rxjava2/src/main/java/com/example/RxJava2Controller.java
@@ -2,6 +2,10 @@ package com.example;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +17,10 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-public class UserController {
+public class RxJava2Controller {
     private final UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping(path = "/users")
     public Flowable<User> findAll() {
         return userService.findAll();
     }
@@ -25,5 +29,19 @@ public class UserController {
     public Single<User> getById(@PathVariable int id) {
         return userService.findById(id)
                           .switchIfEmpty(Single.error(new ResponseStatusException(NOT_FOUND)));
+    }
+
+    @GetMapping(path = "/text_event_stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flowable<String> textEventStream() {
+        return Flowable.range(1, 10)
+                       .map(i -> "hello" + i)
+                       .delay(1000, TimeUnit.MILLISECONDS);
+    }
+
+    @GetMapping(path = "/json_stream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flowable<Map<String, String>> jsonStream() {
+        return Flowable.range(1, 10)
+                       .map(i -> Map.of("message", "hello" + i))
+                       .delay(1000, TimeUnit.MILLISECONDS);
     }
 }
