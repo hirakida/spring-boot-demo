f1046272898501a396ac1f33b8867d794e50d9e8
hirakida <12070156+hirakida@users.noreply.github.com>
2020-12-24 22:29:44 +0900

    Update

--- spring-webflux/websocket/README.md
+++
@@ -1,2 +0,0 @@
-
-https://docs.spring.io/spring-framework/docs/5.3.1/reference/html/web-reactive.html#webflux-websocket
--- websocket/websocket-reactive/src/main/java/com/example/config/WebSocketConfig.java
+++ websocket/websocket-reactive/src/main/java/com/example/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.config;
 
 import java.util.Map;
 
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+
+import com.example.GreetingWebSocketHandler;
 
 @Configuration
 public class WebSocketConfig {
