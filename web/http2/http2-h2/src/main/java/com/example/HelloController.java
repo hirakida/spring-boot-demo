c4d5d4f86798335a09dba54bfb7145ef9f0988a1
hirakida <12070156+hirakida@users.noreply.github.com>
2020-12-26 16:26:36 +0900

    Update

--- web/http2/http2-h2/src/main/java/com/example/HelloController.java
+++ web/http2/http2-h2/src/main/java/com/example/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, HTTP/2";
+        return "Hello, HTTP/2!";
     }
 }
--- web/https/https/README.md
+++
@@ -1,6 +0,0 @@
-
-```
-$ keytool -genkey -alias tomcat -storetype PKCS12 -keyalg RSA -keystore keystore.p12
-```
-
-https://localhost:8443/
