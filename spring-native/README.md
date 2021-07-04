e9a7a99c8e84c969632422fc5a4e52bace6b7712
hirakida <hirakida29@gmail.com>
2021-07-04 12:53:30 +0900

    Update

---
+++ spring-native/README.md
@@ -0,0 +1,11 @@
+
+https://docs.spring.io/spring-native/docs/current/reference/htmlsingle/  
+
+```
+$ sdk install java 21.1.0.r11-grl
+$ sdk use java 21.1.0.r11-grl
+$ gu install native-image
+
+$ ./gradlew nativeBuild
+$ build/native/spring-native-demo
+```
---
+++ spring-native/src/main/java/com/example/Application.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello!");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
