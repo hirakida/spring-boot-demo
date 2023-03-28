7e7f48388c257f86cbf5a9893b5b1ab3bdfa3b64
hirakida <hirakida29@gmail.com>
2023-03-29 00:11:01 +0900

    Update

--- caching/README.md
+++ caching/README.md
@@ -1,3 +1,2 @@
 
 https://docs.spring.io/spring-boot/docs/3.0.x/reference/html/io.html#io.caching
-
---
+++ caching/cache-cache2k/src/main/java/com/example/UserController.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @DeleteMapping
+    public void evictCache() {
+        userService.evictCache();
+    }
+}
