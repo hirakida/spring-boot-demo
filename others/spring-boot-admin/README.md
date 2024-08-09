35b2a08b1ae6a57582fe0be1f382f1b766950d6b
hirakida <hirakida29@gmail.com>
2024-08-09 13:05:10 +0900

    Update spring-boot-admin

--- others/spring-boot-admin/README.md
+++
@@ -1,4 +0,0 @@
-
-https://codecentric.github.io/spring-boot-admin/current/  
-
-http://localhost:8080/applications  
--- spring-boot-admin/spring-boot-admin-server/src/main/java/com/example/Application.java
+++ spring-boot-admin/spring-boot-admin-server/src/main/java/com/example/Application.java
@@ -1,16 +1,13 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
