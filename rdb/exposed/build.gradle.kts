5f533a68d57dba36e8c1655c6236b0eb33cfd5fc
hirakida <hirakida29@gmail.com>
2025-11-02 21:01:40 +0900

    Update

--- rdb/exposed/build.gradle.kts
+++ rdb/exposed/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation(libs.org.jetbrains.exposed.exposed.spring.boot.starter)
+    implementation(libs.exposed.spring.boot.starter)
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
